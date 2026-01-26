ba19cf71cf5d24ea054d518a13ba11d86d602a74
jibankumarpanda <pandajiban331@gmail.com>
2026-01-26 13:16:18 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,12 @@
+import express from "express"
+import cors from "cors"
+import reviews from "./api/reviews.route.js"
+
+const app = express()//express is a function that returns an express application
+app.use(cors())//app.use(cors()) is a middleware that is used to enable cors
+app.use(express.json())//app.use(express.json()) is a middleware that is used to parse the json
+app.use("/api/v1/reviews", reviews)//app.use("/api/v1/reviews", reviews) is a middleware that is used to handle the reviews
+app.use("*", (req, res) => {
+    res.status(404).json({ error: "Not found" })})//app.use("*", (req, res) => {res.status(404).json({ error: "Not found" })}) is a middleware that is used to handle the not found
+
+export default app    
