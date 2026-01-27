f31bc455964fd0175736e42905445c390c019504
jibankumarpanda <pandajiban331@gmail.com>
2026-01-27 19:45:56 +0530

    DAO

--- Backend/dao/reviewDAO.js
+++ Backend/dao/reviewDAO.js
@@ -17,7 +17,58 @@ export default class ReviewDAO{//ReviewDAO is a class that is used to handle the
             console.error(`Unable to establish collection handles in reviewDAO: ${e}`)
         }
     }
-    
+    static async addReview(movieId, user, review){
+        try{
+            const reviewDoc = {
+                movieId: movieId,
+                user: user,
+                review: review,
+            }
+            return await reviews.insertOne(reviewDoc)
+        }
+        catch(e){
+            console.error(`Unable to add review: ${e}`)
+            return {error: e}
+        }
+    }
+    static async getReview(reviewID){
+        try{
+            return await reviews.findOne({
+                _id: new objectID(reviewID)
+            })
+        }
+        catch(e){
+            console.error(`Unable to get review: ${e}`)
+            return {error: e}
+        }
+    }
+    static async updateReview(reviewID, user, review){
+        console.log("rev", reviewID)
+        try{
+            const updateResponse = await reviews.updateOne(
+                { _id: new objectID(reviewID) },
+                { $set: { user: user, review: review } }
+            )
+            return updateResponse
+        }
+        catch(e){
+            console.error(`Unable to update review: ${e}`)
+            return {error: e}
+        }
+    }
+    static async deleteReview(reviewID){
+        try{
+            const deleteResponse = await reviews.deleteOne({
+                _id:objectID(reviewId),
+            })
+            return deleteResponse
+        }
+        catch(e){
+            console.error(`Unable to delete review: ${e}`)
+            return {error: e}
+        }
+        
+    }
 }
 
     
